a65bcf2279bd0d59aad9903a7bd080335fd376b2
Hakim109 <hkhk19019@gmail.com>
2023-07-19 14:25:45 -0700

    print 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 /**
-* _islower - check if c is lowercase
-* @c: carctere to check
-* Return: 1 if c is lowercase, 0 otherwise.
+* print_to_98 - print to 98
+* @n: carctere to print
+* Return: alwyas void
 */
 void print_to_98(int n)
 {
-	    if (n <= 98) {
-        for (; n <= 98; n++) {
-            printf("%d", n);
-            if (n != 98) {
-                printf(", ");
-            }
-        }
-    } else {
-        for (; n >= 98; n--) {
-            printf("%d", n);
-            if (n != 98) {
-                printf(", ");
-            }
-        }
-    }
-    printf("\n");
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
 }
