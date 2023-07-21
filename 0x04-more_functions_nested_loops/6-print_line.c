a8bd308d014023d9efe249285103e9bf37cdcbc8
Hakim109 <hkhk19019@gmail.com>
2023-07-20 17:41:47 -0700

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,12 +10,18 @@
 
 void print_line(int n)
 {
-	int c;
-
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (c = 0; c < n; c++)
-			_putchar('_');
+		_putchar('\n');
 	}
+	else
+	{
+
+	int c;
+		for (c = 0; c < n; c++)
+		{
+			_putchar('_');
+		}
 	_putchar('\n');
+}
 }
