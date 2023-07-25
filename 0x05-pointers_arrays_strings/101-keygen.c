6a5c2a53af4fe60a47198f44533d89de5657242e
Hakim109 <hkhk19019@gmail.com>
2023-07-25 16:15:48 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * generate_password - Generates a random password
+  * generate_password - Generates a random password
  * @length: The length of the password to be generated
  *
  * Return: The generated password
@@ -17,30 +16,30 @@ char *generate_password(int length) {
 
     int i;
     for (i = 0; i < length; i++) {
-        int type = rand() % 3; /* Generate a random number to determine the character type */
+        int type = rand() % 3;
 
-        if (type == 0) /* Uppercase letter*/
+        if (type == 0)
             password[i] = 'A' + rand() % 26;
-        else if (type == 1) // Lowercase letter
+        else if (type == 1)
             password[i] = 'a' + rand() % 26;
-        else // Digit
+        else
             password[i] = '0' + rand() % 10;
     }
 
-    password[length] = '\0'; /* Null-terminate the string*/
+    password[length] = '\0';
     return password;
 }
 
 int main(void) {
-    srand(time(NULL)); // Seed the random number generator with the current time
+    srand(time(NULL));
 
-    int password_length = 10; // You can change the length of the password here
+    int password_length = 10;
 
     char *password = generate_password(password_length);
 
     printf("Generated Password: %s\n", password);
 
-    free(password); // Remember to free the allocated memory
+    free(password);
 
     return 0;
 }
