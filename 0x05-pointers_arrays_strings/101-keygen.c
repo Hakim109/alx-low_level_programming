b83800dfd08c11bcd576924e921675a9048b0429
Hakim109 <hkhk19019@gmail.com>
2023-07-25 17:47:51 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,22 +10,24 @@ Return: Always 0
 */
 int main(void)
 {
-char password[84];
-int i, sum, diff;
+	int pass[100];
+	int i, sum, n;
 
-srand(time(NULL));
+	srand(time(NULL));
 
-for (i = 0, sum = 0; sum < 2772 - 122; i++)
-{
-password[i] = rand() % 94 + 33;
-sum += password[i];
-}
-
-diff = sum - 2772 + 122;
-password[i] = diff;
-
-printf("%s", password);
-
-return (0);
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if (( 2772 - sum) - '0' < 78)
+		{
+			n = sum - 2772 - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
 }
 
