4b2bd2d7424d41d26b0690c84f634f46c63f4e17
Hakim109 <hkhk19019@gmail.com>
2023-07-27 13:47:53 -0700

    101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,31 +6,31 @@
  */
 void print_number(int n)
 {
-    int divisor = 1;
-    int temp = n;
-    int digit;
+	int divisor = 1;
+	int temp = n;
+	int digit;
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
-    if (n < 0)
-    {
-        _putchar('-');
-        temp = -n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		temp = -n;
+	}
 
-    while (temp / divisor >= 10)
-        divisor *= 10;
+	while (temp / divisor >= 10)
+		divisor *= 10;
 
-    while (divisor != 0)
-    {
-        digit = (temp / divisor);
-        _putchar(digit + '0');
-        temp %= divisor;
-        divisor /= 10;
-    }
+	while (divisor != 0)
+	{
+		digit = (temp / divisor);
+		_putchar(digit + '0');
+		temp %= divisor;
+		divisor /= 10;
+	}
 }
 
