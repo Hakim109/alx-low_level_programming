64aaff2d668474f7bbd154d711729b7903382265
Hakim109 <hkhk19019@gmail.com>
2023-07-27 13:30:00 -0700

    101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+	int divisor = 1;
+	int temp = n;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		temp = -n;
+	}
+
+	while (temp / divisor >= 10)
+		divisor *= 10;
+
+	while (divisor != 0)
+	{
+		_putchar((temp / divisor) + '0');
+		temp %= divisor;
+		divisor /= 10;
+	}
+}
+
