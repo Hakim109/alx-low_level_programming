2e94d42dfc87e9193129359d2180c35c1e2b3eef
Hakim109 <hkhk19019@gmail.com>
2023-08-01 11:46:23 -0700

    print diag

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,21 +7,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, k, l, m;
+	int i , diag1 = 0, diag2 = 0;
 
-	k = 0;
-	l = 0;
-	m = 0;
 	for (i = 0; i < size; i++)
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-				l += a[k];
-
-			else if (i + j == size - 1)
-				m += a[k];
-
-			k++;
-		}
-	printf("%i, %i\n", l, m);
+	{
+		diag1 += a[i * size + i];
+		diag2 += a[i * size + (size - 1 - i)];
+	}
+	printf("%i, %i\n", diag1, diag2);
 }
