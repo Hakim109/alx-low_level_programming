e7f6ccfa202aad83ed429e94f15d811898b56d62
Hakim109 <hkhk19019@gmail.com>
2023-08-06 06:43:15 -0700

    2 args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,9 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int i;
-
-	/* Start from 1 to skip the program name (argv[0])*/
-	for (i = 1; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
