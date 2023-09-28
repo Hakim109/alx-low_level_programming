c218bf6472324cd0b461d71fbb08f6338bda35f4
Hakim109 <hkhk19019@gmail.com>
2023-09-28 11:11:33 -0700

    update task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,45 +1,32 @@
 #include "main.h"
 
-int actual_sqrt_recursion(int n, int i);
+int ntr_sqrt_recursion(int n, int i);
 
 /**
- * _sqrt_recursion_hlp - function performs a binary search
- * @n: number to square
- * @l: lower value
- * @h: high value
+ * _sqrt_recursion - returns the natural square of a number
+ * @n: number to calculate the square
  *
- * Return: the resuls
- */
-int _sqrt_recursion_hlp(int n, int l, int h)
-{
-	int j = 0;
-	int sqr = 0;
-
-	if (l > h)
-		return (h);
-
-	j = l + (h - l) / 2;
-	sqr = j * j;
-
-	if (sqr == n)
-		return (j);
-
-	if (sqr < n)
-	return (_sqrt_recursion_hlp(n, j + 1, h));
-	else
-	return (_sqrt_recursion_hlp(n, l, j - 1));
-}
-/**
- *_sqrt_recursion - recurses to find the natural square root of a number
- * @n: number to calculate the sqaure root of
- *
- * Return: the resuls
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-	return (_sqrt_recursion_hlp(n, 0, n / 2));
+	return (ntr_sqrt_recursion(n, 0));
+}
+
+/**
+ * ntr_sqrt_recursion - recurses to find the natural sqrt
+ * @n: number to calculate the sqaure
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int ntr_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (ntr_sqrt_recursion(n, i + 1));
 }
