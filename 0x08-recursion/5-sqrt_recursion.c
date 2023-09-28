db84fb98800be100ea0a6b7b6711031e88de17dd
Hakim109 <hkhk19019@gmail.com>
2023-09-28 10:34:24 -0700

    update task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,29 +1,45 @@
 #include "main.h"
 
+int actual_sqrt_recursion(int n, int i);
+
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number to calculate squart of
+ * _sqrt_recursion_hlp - function performs a binary search
+ * @n: number to square
+ * @l: lower value
+ * @h: high value
  *
- * Return: the resulting square n
+ * Return: the resuls
+ */
+int _sqrt_recursion_hlp(int n, int l, int h)
+{
+	int j = 0;
+	int sqr = 0;
+
+	if (l > h)
+		return (h);
+
+	j = l + (h - l) / 2;
+	sqr = j * j;
+
+	if (sqr == n)
+		return (j);
+
+	if (sqr < n)
+	return (_sqrt_recursion_hlp(n, j + 1, h));
+	else
+	return (_sqrt_recursion_hlp(n, l, j - 1));
+}
+/**
+ *_sqrt_recursion - recurses to find the natural square root of a number
+ * @n: number to calculate the sqaure root of
+ *
+ * Return: the resuls
  */
 int _sqrt_recursion(int n)
 {
-	int j = 0;
-
 	if (n < 0)
-	{
 		return (-1);
-	}
 	if (n == 0 || n == 1)
-	{
 		return (n);
-	}
-	for (j = 1; j <= n / 2; j++)
-	{
-		if (j * j > n)
-			return (-1);
-		if (j * j == n)
-			return (j);
-	}
-		return (j - 1);
+	return (_sqrt_recursion_hlp(n, 0, n / 2));
 }
