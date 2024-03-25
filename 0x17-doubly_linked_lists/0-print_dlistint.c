4d377fec343039eda1bb6ef9490137196eb930c1
Hakim109 <hkhk19019@gmail.com>
2024-03-25 10:50:28 +0100

    task0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+  * print_dlistint - prints a doubly linked list
+  * @h: Head list
+  * Return: Number of items in the linked list
+  */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (!h)
+		return(0);
+	while (h)
+	{
+		print("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
