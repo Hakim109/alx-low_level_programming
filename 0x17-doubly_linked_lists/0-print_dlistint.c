ff6ec1740efad33b5ed5db4274aeed0a75ac2ad8
Hakim109 <hkhk19019@gmail.com>
2024-04-01 10:12:19 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,10 +9,10 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count = 0;
 
 	if (!h)
-		return(0);
+		return (0);
 	while (h)
 	{
-		print("%d\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
